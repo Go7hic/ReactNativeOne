a378f27fa087b5726a3438aac5a21278c82afb0c
Go7hic <gtfx0209@qq.com>
2016-07-03 01:28:23 +0800

    update api

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -12,42 +7,19 @@ import {
   View
 } from 'react-native';
 
+import { Provider } from 'react-redux'
+import Navigation from './src/components/Navigation'
+import configureStore from './src/store/configureStore'
+const store = configureStore()
 class ReactNativeOne extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ReactNativeOne', () => ReactNativeOne);
---
+++ src/config/api.js
@@ -0,0 +1,21 @@
+const baseUrl = 'http://v3.wufazhuce.com:8000/api'
+const api = {
+  homePage: 'hp/more/', //首页，返回过去十天的
+
+  readingCarousel: '/reading/carousel',//阅读页面的幻灯片
+  readingIndex: '/reading/index/', //阅读列表
+  essay2: '/essay/', //短篇
+  essayRelated2: 'elated/essay/', //短篇相关
+  essayComment2: 'comment/praiseandtime/essay/', //短篇相关
+  serialcontent: '/serialcontent/', //连载
+  question: '/question/', //问题
+
+  carouselList: '/reading/carousel/',
+  musicIdList: '/music/idlist/', //音乐 id列表
+  musicDetail: 'music/detail/', //音乐详情
+  musicRelated: 'related/music/', //相关音乐
+  musicComment: 'comment/praiseandtime/music/', //音乐评论
+
+  movieList: '/movie/list/', // 电影列表
+  movieDetail: '/movie/detail/' // 电影详情
+ }
